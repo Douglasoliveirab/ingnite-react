880d184163743519de9446e96ef445e419aff8a4
DouglasThreeo <douglas.oliveira@threeo.com.br>
2024-05-15 23:30:23 -0300

    style: sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,19 +1,25 @@
 import styleSidebar from './Sidebar.module.css';
 import capa from '../../assets/capa.jpeg';
-export function Sidebar(){
- return(
-    <anside className={styleSidebar.sidebar}>
-      <img src={capa} alt={'foto de capa'} />
+import imgProfile from '../../assets/profile.jpeg'
+import { PencilLine } from 'phosphor-react'
 
+export function Sidebar() {
+  return (
+    <anside className={styleSidebar.sidebar}>
+      <img src={capa} alt={'foto de capa'} className={styleSidebar.imgCapa} />
+      <div className={styleSidebar.profile} >
+        <img src={imgProfile} alt={"profile"} className={styleSidebar.avatar} />
+      </div>
       <div className={styleSidebar.profile}>
         <strong>Douglas oliveira</strong>
         <span>Full Stack Developer</span>
       </div>
       <footer>
         <a href='#'>
-            Editar seu perfil
+          <PencilLine size={20} />
+          Editar seu perfil
         </a>
       </footer>
     </anside>
- )   
+  )
 }
