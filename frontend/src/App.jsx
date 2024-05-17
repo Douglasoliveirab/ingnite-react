2cbc95603bc6039d57fb812595a8810e47e245a4
DouglasThreeo <douglas.oliveira@threeo.com.br>
2024-05-16 23:33:50 -0300

    style: component de post

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/header/Header";
+import { Sidebar } from "./components/sidebar/Sidebar";
 import './global.css';
 import styles from './App.module.css';
-import { Sidebar } from "./components/sidebar/Sidebar";
 
 export function App() {
 
@@ -12,14 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Douglas oliveira"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, tenetur. Ea reprehenderit autem ipsa vitae ratione molestias nesciunt sit incidunt velit enim? Quas veniam modi amet nihil itaque! Eveniet, sapiente?"
-          />
-          <Post
-            author="Daniela oliveira"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque, tenetur. Ea reprehenderit autem ipsa vitae ratione molestias nesciunt sit incidunt velit enim? Quas veniam modi amet nihil itaque! Eveniet, sapiente?"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,24 @@
+import stylePost from './Post.module.css';
+import imgProfile from '../assets/profile.jpeg'
+
+export function Post() {
+    return (
+        <article className={stylePost.post}>
+            <header className={stylePost.author}>
+                <div className={stylePost.authorInfo}>
+                    <img src={imgProfile} />
+                    <div>
+                        <strong>Douglas Oliveira</strong>
+                        <span>Full Stack Developer</span>
+                    </div>
+                </div>
+                <time title='15 de Maio ás 08:30' dateTime='2024-05-15'> Publicado a 1h</time>
+            </header>
+            <div className={stylePost.content}>
+                <p>
+                    <a href="#">Acabei de subir mais um prtojeto </a>
+                </p>
+            </div>
+        </article>
+    )
+}
